2d84f5fe2dc7293f0559f030943e8aa400a06408
Sivcan Singh <sivcansingh@me.com>
2016-12-31 14:39:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+##Fresh Tomatoes
+- Project 1 under the Full Stack Web Developer Nanodegree at Udacity
+
+### Details
+- Backend : Python
+- Shows different movies, their trailers, plot and posters.
+
+
---
+++ entertainment_center.py
@@ -0,0 +1,29 @@
+from media import Movie
+import fresh_tomatoes
+
+toy_story = Movie("Toy Story","A story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = Movie("Avatar","A story about an alien on a planet","https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+godfather = Movie("Godfather","Godfather. Need I say more?","https://s-media-cache-ak0.pinimg.com/originals/f6/9e/c6/f69ec6bc013826e2988c4865f5a1537c.jpg","https://www.youtube.com/watch?v=sY1S34973zA")
+
+transformers = Movie("Transformers", "The noble Autobots and devious Decepticons!","http://www.firstshowing.net/img/optimus-poster-big.jpg","https://www.youtube.com/watch?v=dxQxgAfNzyE")
+
+dont_breathe = Movie("Don't Breathe", "A blind man and his story.","https://upload.wikimedia.org/wikipedia/en/4/41/Don't_Breathe_(2016_film).png","https://www.youtube.com/watch?v=76yBTNDB6vU")
+
+shawshank = Movie("Shawshank Redemption","Watch it. To Believe it. ;)","http://www.impawards.com/1994/posters/shawshank_redemption_ver1.jpg","https://www.youtube.com/watch?v=6hB3S9bIaco")
+
+movies = [toy_story, avatar, godfather, transformers, dont_breathe, shawshank]
+
+fresh_tomatoes.open_movies_page(movies)
+
+#print(Movie.VALID_RATINGS)
+#print(Movie.__doc__)
+#print(Movie.__name__)
+#print(Movie.__module__)
+
+
+
+
+
+
